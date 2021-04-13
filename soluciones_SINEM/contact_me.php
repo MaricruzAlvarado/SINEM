55aa49ff02388101f3069f700e57fec79c6e80ac
MaricruzAlvarado <42251437+MaricruzAlvarado@users.noreply.github.com>
2021-04-13 02:41:47 -0500

    Web page SINEM

---
+++ soluciones_SINEM/contact_me.php
@@ -0,0 +1,7 @@
+﻿if(isset($_POST['name'],$_POST['email'],$_POST['message'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message']; 
+
+    echo $name; // then echo $email and then $message
+}
---
+++ soluciones_SINEM/email.php
@@ -0,0 +1,15 @@
+﻿<?php
+$_name = $_POST['name'];
+$_email = $_POST['email'];
+$_msg = $_POST['msg'];
+$_email = $_POST['email'];
+
+
+$_to = "contacto@solucionesinem.com.mx";
+$_subject = "Contacto Soluciones SINEM";
+$_body = "Name: ".$_name."\nEmail: ".$_email."\nMessage: ".$_msg;
+$_headers = "From: " . $_email;
+
+//send email
+@mail($_to, $_subject, $_body, $_headers);
+?>
